a5be560f5d92088774b33d4bd42dfad8e7f31780
Ferdinand Kramer <f.kramer@tricks.de>
2024-05-16 18:21:15 +0200

    attempt to fix requirability 2/x

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ const dynamicSpacing = plugin.withOptions(
         extend: {
           spacing: (theme) => {
             return spacing(theme('screens'))
+          },
         },
       },
     }
